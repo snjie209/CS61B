6c28a9d3b1708b8664d5089e6fbedaa1ae0e564c
Samba Njie Jr <sambamamba@Sambas-MacBook-Pro.local>
2018-04-23 22:39:21 -0700

    LeapYear callable from command line

--- lab01/LeapYear.java
+++ lab01/LeapYear.java
@@ -29,4 +29,17 @@ public class LeapYear {
             }
         }
     }
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        } else if (year % 4 == 0) {
+            if (year % 100 != 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
 }
